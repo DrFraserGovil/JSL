b7fe25509831e813ce051b1b1c8ab0a2264e36e0
Jack Fraser <jacktfraser@physics.ox.ac.uk>
2021-06-10 16:09:41 +0100

    Added files

---
+++ FileIO/LineReader.h
@@ -0,0 +1,49 @@
+#pragma once
+
+
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <dirent.h>
+
+#include "../Strings/split.h"
+#define forLineInFile(macroFileName, ...)\
+{								\
+	do 							\
+	{							\
+		std::ifstream macroFile(macroFileName);	\
+		if (!macroFile.is_open())	\
+		{							\
+			std::cout << "\n\nERROR: Could not find the file '" << macroFileName << ".\n\nPlease provide a valid directory within the Output directory\n\n " << std::endl;	\
+			exit(1);				\
+		}							\
+		std::string FILE_LINE;				\
+		while (getline(macroFile,FILE_LINE))	\
+		{							\
+			__VA_ARGS__				\
+		}							\
+		macroFile.close();			\
+	} while(0);						\
+}									\
+
+#define forLineVectorInFile(macroFileName, token,...)\
+{								\
+	do 							\
+	{							\
+		std::ifstream macroFile(macroFileName);	\
+		if (!macroFile.is_open())	\
+		{							\
+			std::cout << "\n\nERROR: Could not find the file '" << macroFileName << ".\n\nPlease provide a valid directory within the Output directory\n\n " << std::endl;	\
+			exit(1);				\
+		}							\
+		std::string FILE_LINE;				\
+		while (getline(macroFile,FILE_LINE))	\
+		{							\
+			std::vector<std::string> FILE_LINE_VECTOR = JSL::split(FILE_LINE,token);	\
+			__VA_ARGS__;				\
+		}							\
+		macroFile.close();			\
+	} while(0);						\
+}		
