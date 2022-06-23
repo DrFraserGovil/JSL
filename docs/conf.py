b486552e93daed0f00093b1cded4270cd442c71f
Jack <jf20@sanger.ac.uk>
2022-06-23 15:53:12 +0100

    Added neat thing to indexer, to stip out JSL::

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 highlight_language = 'c++'
+
+cpp_index_common_prefix = ['JSL::']
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
