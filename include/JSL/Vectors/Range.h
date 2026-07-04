c0832ef29a68101d9ed92b03da10992688be531b
Jack Fraser-Govil <j.fraser-govil@bham.ac.uk>
2026-07-04 20:38:19 +0100

    Upgraded the linespace algorithms to work on arbitrary return types, and renamed them 'ranges', so as not to be hanging too close to numpy

---
+++ include/JSL/Vectors/Range.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Cast.h"
+#include <JSL/Concepts/ranges.h>
+#include <vector>
+namespace JSL::Vector
+{
+
+	namespace internal
+	{
+		void verifyRange(double begin, double end, size_t res);
+	}
+
+	/* @brief Generates a vector of doubles evenly spaced on the domain (begin,end)
+		@details There is no requirement that ``begin < end``, but we do require ``begin != end``
+		@tparam
+		@param begin The starting element of the sequence
+		@param end The final element of the sequence
+		@param resolution The number of elements in the output vector
+		@return A vector of length ``resolution``
+		@throws runtime_error if ``resolution < 2``
+		@throws runtime_error if |begin-end| < 1e-14
+	*/
+	template <Concept::SearchableRange R = std::vector<double>>
+		requires std::convertible_to<double, JSL::Concept::RangeInternalType<R>>
+	R range(double begin, double end, size_t resolution = 100)
+	{
+		internal::verifyRange(begin, end, resolution);
+		double delta = (end - begin) / (resolution - 1);
+
+		R out;
+		if constexpr (requires { out.reserve(resolution); })
+		{
+			out.reserve(resolution);
+		}
+		for (size_t i = 0; i < resolution - 1; ++i)
+		{
+			out.insert(out.end(), begin + i * delta);
+		}
+		out.insert(out.end(),end); // ensure the end is exactly included
+		return out;
+	}
+
+} // namespace JSL::Vector
---
+++ src/Vectors/Range.cpp
@@ -0,0 +1,13 @@
+#include <JSL/Vectors/Linspace.h>
+#include <JSL/internal/error.h>
+void JSL::Vector::internal::verifyRange(double begin, double end, size_t resolution)
+{
+	if (resolution < 2)
+	{
+		JSL::internal::FatalError("Bad linspace", JSL_LOCATION) << "A linspace cannot have " << resolution << " elements (must be at least 2)";
+	}
+	if (std::abs(end - begin) < 1e-14)
+	{
+		JSL::internal::FatalError("Bad linspace", JSL_LOCATION) << "A linspace must have endpoints which are separated by more than a machine-epsilon";
+	}
+}
