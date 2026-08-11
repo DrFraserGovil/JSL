ed383c80b8002c2549ad584c56cc54e16a9672ce
Jack Fraser-Govil <j.fraser-govil@bham.ac.uk>
2026-08-11 14:38:57 +0100

    Properly initialised WSA now

--- include/JSL/Async/Socket/SocketBase.h
+++ include/JSL/Async/Socket/SocketBase.h
@@ -3,9 +3,46 @@
 #include <filesystem>
 namespace JSL::Async::Socket::internal
 {
+#ifdef WINMODE
+	class WinsockContext
+	{
+	  public:
+		static void Ensure()
+		{
+			static WinsockContext instance; // constructed exactly once, thread-safe per C++11 static-local rules
+			(void)instance;
+		}
+
+	  private:
+		WinsockContext()
+		{
+			WSADATA data;
+			int result = WSAStartup(MAKEWORD(2, 2), &data);
+			if (result != 0)
+			{
+				JSL::internal::LibraryError("Failed to initialise Winsock", JSL_LOCATION)
+					<< "WSAStartup failed with error " << result;
+			}
+		}
+		~WinsockContext()
+		{
+			WSACleanup();
+		}
+		WinsockContext(const WinsockContext &) = delete;
+		WinsockContext &operator=(const WinsockContext &) = delete;
+	};
+#else
+	class WinsockContext
+	{
+	  public:
+		static void Ensure() {} // nothing to do on POSIX
+	};
+#endif
 	class SocketBase
 	{
 	  public:
+		SocketBase() { WinsockContext::Ensure(); }
+
 	  protected:
 		std::string Identifier;
 		socket_t FileDescriptor = INVALID_SOCKET_VAL;
