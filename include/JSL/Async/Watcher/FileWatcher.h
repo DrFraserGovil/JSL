329158dea0dec08e5c60c1f02d82bad71fc0c126
Jack Fraser-Govil <j.fraser-govil@bham.ac.uk>
2026-08-15 23:43:52 +0100

    Fixed some bugs induced by a copy/paste

--- include/JSL/Async/Watcher/FileWatcher.h
+++ include/JSL/Async/Watcher/FileWatcher.h
@@ -129,7 +129,6 @@ namespace JSL::Async::Watcher
 		int KqueueFd{-1};
 		int ShutdownPipe[2]{-1, -1};
 		std::map<int, std::filesystem::path> WatchMap; // watch descriptor -> absolute directory path
-		bool PlatformWatchFiles = false;
 #endif
 	};
 } // namespace JSL::Async::Watcher
