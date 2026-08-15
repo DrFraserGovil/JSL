5d41c6dcd43f20ed4c19b9cf11196d119378248d
Jack Fraser-Govil <j.fraser-govil@bham.ac.uk>
2026-08-15 23:40:19 +0100

    Added some missing header values to the BSD version

--- include/JSL/Async/Watcher/FileWatcher.h
+++ include/JSL/Async/Watcher/FileWatcher.h
@@ -126,6 +126,10 @@ namespace JSL::Async::Watcher
 #endif
 #ifdef BSDMODE
 		bool PlatformWatchFiles = true;
+		int InotifyFd{-1};
+		int ShutdownPipe[2]{-1, -1};
+		std::map<int, std::filesystem::path> WatchMap; // watch descriptor -> absolute directory path
+		bool PlatformWatchFiles = false;
 #endif
 	};
 } // namespace JSL::Async::Watcher
